11a788e7c92115e8eb41e51a6897632df86955d1
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-11-30 02:08:43 -0500

    Created light and dark theme pallette.

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,28 +1,50 @@
-import { createTheme } from '@mui/material'
-declare module '@mui/material/styles' {
-    interface Palette {
-      HBPNavy: Palette['primary'];
-      Mist: Palette['primary'];
-      DarkGreen: Palette['primary'];
-      HBPBlue: Palette['primary'];
-      YellowGreen: Palette['primary'];
-      SproutGreen: Palette['primary'];
-      White: Palette['primary'];
-      Red: Palette ['primary'];
-      HBPCTA: Palette ['primary'];
-      Teal: Palette ['primary'];
-    }
-    interface PaletteOptions {
-      HBPNavy: PaletteOptions['primary'];
-      Mist: PaletteOptions['primary'];
-      DarkGreen: PaletteOptions['primary'];
-      HBPBlue: PaletteOptions['primary'];
-      YellowGreen: PaletteOptions['primary'];
-      SproutGreen: PaletteOptions['primary'];
-      White: PaletteOptions['primary'];
-      Red: PaletteOptions ['primary'];
-      HBPCTA: PaletteOptions ['primary'];
-      Teal: PaletteOptions ['primary'];
+import { createTheme } from '@mui/material/styles';
+​
+const baseTheme = createTheme({
+  typography: {
+    fontFamily: "'Work Sans', sans-serif",
+    fontSize: 14,
+  }
+})
+​
+const darkTheme = createTheme({
+  ...baseTheme,
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#f9f5f1"
+    },
+    secondary: {
+      main: "#005e78"
+    },
+    background: {
+      default: "#000000"  
+    },
+    common: {
+      black: "#000000",
+      white: "#ffffff"
     }
   }
+})
+const lightTheme = createTheme({
+  ...baseTheme,
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#000000"
+    },
+    secondary: {
+      main: "#005e78"
+    },
+    background: {
+      default: "#f9f5f1"  
+    },
+    common: {
+      black: "#000000",
+      white: "#ffffff"
+    }
+  }
+})
+​
+export { darkTheme, lightTheme }
   
