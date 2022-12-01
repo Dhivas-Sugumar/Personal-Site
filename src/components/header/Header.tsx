c41c5246550f34011ab3db1f010e239879bde556
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-12-01 02:11:12 -0500

    Added Skeletal header.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header : React.FC = () => {
+    return (
+        <div>
+            <p>DHIVAS SUGUMAR</p>
+            <p>SOFTWARE ENGINEER</p>
+        </div>
+    )
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Header from '../components/header/Header'
 
 const IndexPage = () => (
-  <Layout title="dhivas sugumar|software developer">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
+  <Header/>
 )
 
 export default IndexPage
