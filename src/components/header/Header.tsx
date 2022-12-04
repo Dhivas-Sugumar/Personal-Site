ed1b3d96f4224bf72e955b719733b164b20c147e
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-12-04 14:22:14 -0500

    Skeletal header made. STyled typography created.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import { H3 } from "../../styles/styles";
+ 
 const Header : React.FC = () => {
     return (
-        <div>
-            <p>DHIVAS SUGUMAR</p>
-            <p>SOFTWARE ENGINEER</p>
-        </div>
+        <header>
+            <div className="flex flex-row">
+            <H3 className="basis-4/12">DHIVAS SUGUMAR</H3>
+            <H3 className="basis-8/12">SOFTWARE ENGINEER</H3>
+            </div>
+        </header>
     )
 }
 
---
+++ src/styles/styles.ts
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+const H1 = styled.h1`
+  font-size: clamp(1.5em, 9.5vw, 6.5em);
+  letter-spacing: 0.04em;
+  margin: 0;
+`;
+
+const H2 = styled.h2`
+  font-size: clamp(1.5em, 8.2vw, 3.1em);
+  margin: 0;
+`;
+
+const H3 = styled.h3`
+  font-size: clamp(1.3em, 6.6vw, 2.1em);
+  margin: 0;
+`;
+
+const H4 = styled.h4`
+  margin: 0;
+  font-size: 1.3em;
+  letter-spacing: 0.1em;
+`;
+
+const P = styled.p`
+  margin: 0;
+  font-size: 1.1em;
+  line-height: 1.4em;
+`;
+
+export { H1, H2, H3, H4, P };
+
+
