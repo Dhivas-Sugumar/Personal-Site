8051c41b5634484b7f124565aa7ba77383ea3747
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-12-24 18:44:50 -0500

    Added loop text to headline.

--- src/components/headline/Headline.tsx
+++ src/components/headline/Headline.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { H1 } from "../../styles/styles";
+import TextLoop from "../animation-components/TextLoop/TextLoop";
 
 
 const Headline : React.FC = () => {
     return (
         <div id="intro">
             <H1>
-                SOFTWARE ENGINEER
+                <TextLoop texts={
+                    ["SOFTWARE DEVELOPER",
+                     "STUDENT",
+                     "DESIGNER",
+                     "READER",
+                     "SPACE ENTHUSIAST",
+                     "AURORA CHASER",
+                     "TRAVELLER",
+                     "PLANT DAD", 
+                     "PHILOSOPHY ENTHUSIAST" 
+                    ]
+                    } 
+                />
             </H1>
         </div>
     )
