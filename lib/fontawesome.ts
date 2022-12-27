ec954c7e00bc354a8afca315322153f1ad2a8a5f
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-12-26 23:12:36 -0500

    Added each skill.

--- lib/fontawesome.ts
+++ lib/fontawesome.ts
@@ -1,9 +1,31 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {faJava, faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons"
+import {faJava, 
+    faGithub, 
+    faLinkedin,
+    faPython,
+    faHtml5,
+    faCss3,
+    faJsSquare,
+    faGoogleDrive,
+    faReact,
+    faBootstrap,
+    faMicrosoft,
+    faGoogle
+
+} from "@fortawesome/free-brands-svg-icons"
 import {faPaperPlane} from "@fortawesome/free-regular-svg-icons"
 library.add(
     faJava, 
     faGithub,
     faLinkedin,
-    faPaperPlane
+    faPaperPlane,
+    faPython,
+    faHtml5,
+    faCss3,
+    faJsSquare,
+    faGoogleDrive,
+    faReact,
+    faBootstrap,
+    faMicrosoft,
+    faGoogle
 )
--- src/components/resume/skills/Skills.tsx
+++ src/components/resume/skills/Skills.tsx
@@ -14,14 +14,40 @@ const Skills: React.FC = () => {
         <Grid container spacing={2}>
             <Grid item xs={12}>
             <SkillsItem name={"Java"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Python"} logo={<FontAwesomeIcon icon={["fab","python"]} size="3x" />} proficiency={9} />
+            <SkillsItem name={"Ruby"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={9} />
+            <SkillsItem name={"HTML"} logo={<FontAwesomeIcon icon={["fab","html5"]} size="3x" />} proficiency={10} />
+            <SkillsItem name={"CSS"} logo={<FontAwesomeIcon icon={["fab","css3"]} size="3x" />} proficiency={6} />
+            <SkillsItem name={"JavaScript"} logo={<FontAwesomeIcon icon={["fab","js-square"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Typescript"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"SQL"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={5} />
+            <SkillsItem name={"R"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={3} />
+            <SkillsItem name={"C++"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={4} />
             </Grid>
 
         </Grid>
 
         <H4>Software Tools</H4>
 
-        <H4>Frameworks & Libraries</H4>
-        {/* <SkillsItem 
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+            <SkillsItem name={"Ruby Mine"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Visual Studio Code"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Eclipse"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"PyCharm"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"IntelliJ"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"AutoCAD"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Microsoft Suite"} logo={<FontAwesomeIcon icon={["fab","microsoft"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Adobe Suite"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Jupyter Notebook"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Heroku"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Vercel"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Google Cloud"} logo={<FontAwesomeIcon icon={["fab","google"]} size="3x" />} proficiency={8} />
+
+            </Grid>
+
+        </Grid>
+              {/* <SkillsItem 
             type={"Programming Languages"} 
             skills={["Java","Python","Ruby","HTML","CSS","JavaScript", "TypeScript",
             "SQL(familiar)" ,"R(familiar)", "C++(familiar)"
@@ -40,6 +66,24 @@ const Skills: React.FC = () => {
             "Framer Motion","Ruby on Rails", "pandas", "pillow", "scikit learn"
             ]} 
         /> */}
+
+        <H4>Frameworks & Libraries</H4>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+            <SkillsItem name={"React"} logo={<FontAwesomeIcon icon={["fab","react"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"NextJS"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"NestJS"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Bootstrap"} logo={<FontAwesomeIcon icon={["fab","bootstrap"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Material UI"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Framer Motion"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"Ruby on Rails"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"pandas"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"pillow"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+            <SkillsItem name={"scikit learn"} logo={<FontAwesomeIcon icon={["fab","java"]} size="3x" />} proficiency={8} />
+
+            </Grid>
+
+        </Grid>
         </>
         
     )
