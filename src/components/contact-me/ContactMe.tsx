b57ed165fa66eac5e776d713eb18ae95846100de
Dhivas Sugumar <dhivassugumar@gmail.com>
2022-12-30 02:07:40 -0500

    Made contact me link.

--- src/components/contact-me/ContactMe.tsx
+++ src/components/contact-me/ContactMe.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import ContactMeForm from "./ContactMeForm";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { H3 } from "../../styles/styles";
+import { H1} from "../../styles/styles";
 import ContactMeLink from "./ContactMeLink";
 import { Grid } from "@mui/material";
  
 const ContactMe : React.FC = () => {
     return (
         <Grid container spacing={2} id="contact">
+          <Grid item xs={12}>
+          <H1>Contact Me!</H1>
+
+          </Grid>
   <Grid item xs={4}>
     <div>
             <ContactMeLink
-            brand="github"
+            brand="Github"
             link="https://github.com/Dhivas-Sugumar"
-            icon={<FontAwesomeIcon icon={["fab", "github"]}/>}
+            icon={<FontAwesomeIcon icon={["fab", "github"]} size={"3x"}/>}
             />
             <ContactMeLink
             brand="LinkedIn"
             link="https://github.com/Dhivas-Sugumar"
-            icon={<FontAwesomeIcon icon={["fab", "linkedin"]}/>}
+            icon={<FontAwesomeIcon icon={["fab", "linkedin"]} size={"3x"}/>}
             />
             </div>
   </Grid>
--- src/components/contact-me/ContactMeLink.tsx
+++ src/components/contact-me/ContactMeLink.tsx
@@ -1,19 +1,26 @@
 import { Link } from "@mui/material";
 import React from "react";
 import { ContactMeLinkProps } from "../../../lib/types";
+import ContactMe from "./ContactMe";
+import { ContactMeLinkText, StyledContactMeLinkContainer } from "./ContactMeLink.styles";
 
 const ContactMeLink : React.FC<ContactMeLinkProps> = ( {brand, link , icon} ) => {
     return (
-        <div>
             <Link
                 href={link}
                 target="_blank"
+                underline={"none"}
 
             >
+                        <StyledContactMeLinkContainer  whileHover={{ scale: 1.1 }}>
+
+                <ContactMeLinkText>
                 {brand}
+                </ContactMeLinkText>
                 {icon}
+                </StyledContactMeLinkContainer>
+
             </Link>
-        </div>
     )
 }
 
