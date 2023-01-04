cdafb9347cbc54ca51eff08f154a075840325889
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-01-04 00:37:00 -0500

    Added new experiences.

--- src/components/resume/experience/Experience.tsx
+++ src/components/resume/experience/Experience.tsx
@@ -7,14 +7,38 @@ const Experience : React.FC = () => {
         <>
         <H2>Work Experience</H2>
         <ExperienceItem
+            company={"HackBeanpot Inc"}
+            location={"Boston, MA"}
+            title={"Core Tech Team "}
+            startDate={"April 2022"}
+            endDate={"present"}
+            description={[
+                "Contributed to developing an email sender project to reduce friction for the social outreach team when sending personalized emails to multiple recipients. ",
+                "Participated in weekly meetings to make decisions on the technological stack, discuss solutions for problems, and improve code readability. ",
+                "Lead workshops team to develop informative workshops for the hackathon on a wide range of engineering and career topics.",
+            ]} 
+        />
+        <ExperienceItem
+            company={"Khoury College of Computer Science"}
+            location={"Boston, MA"}
+            title={"Computer Science Professional Development for Co-op Teaching Assistant"}
+            startDate={"Jan 2023"}
+            endDate={"present"}
+            description={[
+            ]} 
+        />
+        <ExperienceItem
             company={"Clora"}
             location={"Boston, MA"}
             title={"Full Stack Web-Developer Co-op"}
             startDate={"July 2022"}
-            endDate={"present"}
+            endDate={"Dec 2022"}
             description={[
-                "Participate in bi-weekly sprints which address improvements and bugs for Clora's external and internalwebsite.",
-                "Spearhead implementation of new features such as consultant suggestions, search result paginations,assign an internal rep to project.",
+                "Proposed and spearheaded a project to document the consultant profile taggings AI scripts built using python, sklearn, and xgboost.",
+                "Spearheaded initiatives such as consultant suggestions, search results pagination, and assigning a Clora rep for projects.",
+                "Worked on tickets to improve and bug-fix Clora's marketplace website and the internal facing site.",
+                "Shadowed product development processes to develop 2022 roadmap.",
+                "Contributed to code reviews, architecture decisions, and team processes in Agile scrum sprints.",
             ]} 
         />
         <ExperienceItem
