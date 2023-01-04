7d2d350d114be926889f35dfd7946d62aeb2e3f3
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-01-04 00:40:48 -0500

    Added header for about me section.

--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -2,17 +2,20 @@ import { Grid } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 import Me from "../../assets/me.jpg"
+import { H1, H2 } from "../../styles/styles";
 import { AboutMeContainer, AboutMeText } from "./AboutMe.styles";
 
 const AboutMe : React.FC = () => {
     return (
         <AboutMeContainer>
+        <H1>About Me</H1>
+
         <Grid container spacing={2}>
             <Grid item xs={9}>
             <AboutMeText>
             {`Hi I'm Dhivas, a computer science
             student at Northeastern university and software developer based in Boston.
-            Currently coding at Clora.`}
+            Currently coding at Hackbeanpot and teaching at Northeastern University.`}
         </AboutMeText>
             </Grid>
             <Grid item xs={3}>
