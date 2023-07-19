48588f572d5daebcac4fc503cc5d5734abeb31e5
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-07-19 00:40:37 -0400

    Notes list works correctly.

--- src/components/notes-contents-list/NotesContentsList.tsx
+++ src/components/notes-contents-list/NotesContentsList.tsx
@@ -1,19 +1,24 @@
 import { List, ListItem, ListItemText } from "@mui/material"
 import { notes } from "../../pages/notes/list"
 import Link from "next/link"
+import { NotesContentsListContainer, StyledListItem } from "./NotesContentsList.styles"
 
 
 const NotesContentsList = () => {
     return (
-        <List>
-            {Object.entries(notes).map(([id, title]) => (
-        <ListItem key={id}>
-            <Link href={`/notes/${id}`}>
-            <ListItemText primary={`${id}: ${title}`} />
-            </Link>
-        </ListItem>
-      ))}
-        </List>
+        <NotesContentsListContainer>
+            <List>
+                {Object.entries(notes).map(([id, title]) => (
+                    <StyledListItem key={id}>
+                        <Link href={`/notes/${id}`}>
+                            <ListItemText primary={`${id}: ${title}`} />
+                        </Link>
+                    </StyledListItem>
+                ))}
+            </List>
+
+        </NotesContentsListContainer>
+
     )
 }
 
